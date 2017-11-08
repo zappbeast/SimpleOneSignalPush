aadb24659b5efbfaa4fe402e2f7cce2819ee8ae9
zappbeast <zappcto@gmail.com>
2017-11-09 04:38:58 +0800

    initial commit
    Sending push notification using OneSignal in Python.

---
+++ onesignal.py
@@ -0,0 +1,10 @@
+import requests
+import json
+from .config import REST_API_KEY, APP_ID
+
+def send_push_notification(player_id, message, data):
+    header = {"Content-Type": "application/json; charset=utf-8", "Authorization": "Basic " + REST_API_KEY}
+    payload = {"app_id": APP_ID, "include_player_ids": [player_id], "contents": {"en": message}, "data": data}
+    req = requests.post("https://onesignal.com/api/v1/notifications", headers=header, data=json.dumps(payload))
+    print(req.status_code, req.reason)
+
