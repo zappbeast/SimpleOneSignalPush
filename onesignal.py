01bf55f21f9760e11253034e030d71bd1db2bd91
Zahid Ahmed <zappcto@gmail.com>
2017-11-09 11:04:18 +0800

    fixed name issue

--- onesignal.py
+++ onesignal.py
@@ -4,7 +4,7 @@ from .config import REST_API_KEY, APP_ID
 
 def send_push_notification(player_id, message, additionalData):
     header = {"Content-Type": "application/json; charset=utf-8", "Authorization": "Basic " + REST_API_KEY}
-    payload = {"app_id": APP_ID, "include_player_ids": [player_id], "contents": {"en": message}, "data": addionalData}
+    payload = {"app_id": APP_ID, "include_player_ids": [player_id], "contents": {"en": message}, "data": additionalData}
     req = requests.post("https://onesignal.com/api/v1/notifications", headers=header, data=json.dumps(payload))
     print(req.status_code, req.reason)
 
