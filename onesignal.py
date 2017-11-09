25cafcca23eddb4cda3f345ac7f7ba3fff856df3
Zahid Ahmed <zappcto@gmail.com>
2017-11-09 11:03:26 +0800

    parameter name changed
    parameter name "data" changed to "additionalData".

--- onesignal.py
+++ onesignal.py
@@ -2,9 +2,9 @@ import requests
 import json
 from .config import REST_API_KEY, APP_ID
 
-def send_push_notification(player_id, message, data):
+def send_push_notification(player_id, message, additionalData):
     header = {"Content-Type": "application/json; charset=utf-8", "Authorization": "Basic " + REST_API_KEY}
-    payload = {"app_id": APP_ID, "include_player_ids": [player_id], "contents": {"en": message}, "data": data}
+    payload = {"app_id": APP_ID, "include_player_ids": [player_id], "contents": {"en": message}, "data": addionalData}
     req = requests.post("https://onesignal.com/api/v1/notifications", headers=header, data=json.dumps(payload))
     print(req.status_code, req.reason)
 
